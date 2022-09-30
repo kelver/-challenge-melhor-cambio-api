5b4951215585c693524bd78ad999f6f75dfd76d3
Kelver <kelver_kmv@hotmail.com>
2022-09-30 03:37:33 -0300

    chore: Models User and Repos

---
+++ app/Models/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'username',
+        'avatar_url',
+        'about',
+        'user_id',
+        'repos_count',
+    ];
+
+    public function repos(): HasMany
+    {
+        return $this->hasMany(Repos::class);
+    }
+}
